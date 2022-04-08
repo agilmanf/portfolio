1ffebebae47e32c0dce24f859ef68f1013e6f74e
agilmanf <agilmanf@gmail.com>
2022-04-08 17:57:48 +0700

    adjust the responsiveness

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ const el = document.querySelector(".background");
 const header = document.querySelector("header");
 
 header.addEventListener("mousemove", (e) => {
-  let transform = `scale(1.2) `;
+  let transform = "";
+
+  switch (true) {
+    case document.body.clientWidth > 1280:
+      transform = "scale(1.2)";
+      break;
+    case document.body.clientWidth > 1024:
+      transform = "scale(1.4)";
+      break;
+    case document.body.clientWidth > 768:
+      transform = "scale(1.6)";
+      break;
+    case document.body.clientWidth < 640:
+      transform = "scale(1.8)";
+      break;
+    default:
+      transform = "scale(1.1)";
+  }
+
+  // console.log(transform);
+
   let sensitivity = 0.05;
   let translateX = (e.clientX - window.innerWidth / 2) * sensitivity;
   let translateY = (e.clientY - window.innerHeight / 2) * sensitivity;
   transform += `translate(${translateX}px, ${translateY}px)`;
-  console.log(transform);
   el.style.transform = transform;
 });
